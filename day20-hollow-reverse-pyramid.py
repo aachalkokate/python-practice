41411a605d4e50fd1d31aac067bd21f99f41dbca
Aachal Kokate <kokateaachal@gmail.com>
2026-08-10 23:06:41 +0530

    Add Day 20 star and hollow patterns
    Completed Day 20 Python pattern programs including star diamond, hollow square, hollow pyramid, hollow reverse pyramid, and hollow diamond.

---
+++ day20-hollow-reverse-pyramid.py
@@ -0,0 +1,23 @@
+for i in range(1, 6):
+    for j in range(5-i):
+        print(" ", end="")
+
+    for j in range(2*i-1):
+        if j == 0 or j == 2*i-2 or i==1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
+
+for i in range(4, 0, -1):
+    for j in range(5-i):
+        print(" ", end="")
+
+    for j in range(2*i-1):
+        if j == 0 or j == 2*i-2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
