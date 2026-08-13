d3e985589bb6e24f4753056d23caff698f257e98
Aachal Kokate <kokateaachal@gmail.com>
2026-08-13 22:21:55 +0530

    Add Day 23 number logic programs
    Completed Day 23 Python programs focusing on loops, conditions, arithmetic operations, factorial calculation, and digit counting using for and while loops.

---
+++ day23-factorial.py
@@ -0,0 +1,9 @@
+num=int(input("enter your number:"))
+count=0
+while num != 0:
+        count=count+1
+        num=num//10
+print("number of digits=",count)
+print()
+    
+        
---
+++ day23-sum-of-numbers.py
@@ -0,0 +1,8 @@
+num=int(input("enter your number:"))
+sum=0
+for i in range(1, num + 1):
+   if i%2==0:
+     sum= sum + i
+
+print("sum=", sum)
+print()
