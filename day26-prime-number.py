40628e333c2bafa9313092b45dc1cafb6fc0f150
Aachal Kokate <kokateaachal@gmail.com>
2026-08-17 22:55:18 +0530

    Add Day 26 number logic programs

---
+++ day26-prime-number.py
@@ -0,0 +1,10 @@
+start=int(input("enter starting number:"))
+end=int(input("enter ending number:"))
+
+for num in range (start = end + 1)
+count = 0
+for i in range(1,num + 1):
+  if num % i==0
+  count=count+1
+if count==2:
+    prime (num)
